99fdd3621357bbd62e7c43fccc66c758fb06ee99
CardinalRichelieu <ben.cartoys@gmail.com>
2020-11-15 21:05:44 -0800

    Initial commit

---
+++ inventory.py
@@ -0,0 +1,14 @@
+class Location():
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+
+l1 = Location("Kitchen", "downstairs")
+print(l1.level)
+
+
+
+
+
+
